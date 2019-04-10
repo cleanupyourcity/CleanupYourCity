5c6bf5c7a02eaba9ad03afca0f381a87cd3176d2
Diana Danvers <itranscode@Dianas-MacBook-Pro.local>
2019-04-10 15:49:47 -0700

    Firebase added, No .DS_STORE pt2

---
+++ CleanUpYourCity/Pods/Target Support Files/Pods-CleanUpYourCityTests/Pods-CleanUpYourCityTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CleanUpYourCityTests : NSObject
+@end
+@implementation PodsDummy_Pods_CleanUpYourCityTests
+@end
---
+++ CleanUpYourCity/Pods/Target Support Files/Pods-CleanUpYourCityUITests/Pods-CleanUpYourCityUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CleanUpYourCityUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CleanUpYourCityUITests
+@end
