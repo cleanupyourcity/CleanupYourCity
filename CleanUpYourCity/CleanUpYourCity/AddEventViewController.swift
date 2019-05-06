5b4d02631a4526b14bc4e2aa5b7744532ded9a15
Jared Long <long3686@m506-118-26450.local>
2019-05-06 15:55:02 -0700

    jared

--- CleanUpYourCity/CleanUpYourCity/AddEventViewController.swift
+++ CleanUpYourCity/CleanUpYourCity/AddEventViewController.swift
@@ -7,13 +7,25 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import Firebase
 
 class AddEventViewController: UIViewController {
 
+    var ref:DatabaseReference?
+    
+    let userID = Auth.auth().currentUser?.uid;
+    
+    @IBOutlet weak var eventNameField: UITextField!
+    
+    @IBOutlet weak var eventDescriptionField: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        ref = Database.database().reference()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +33,19 @@ class AddEventViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func addEvent(_ sender: Any) {
+        
+        //Post event data to firebase
+        let key = ref?.childByAutoId().key
+        
+        //creating artist with the given values
+        let event = ["eventAuthorID":userID,
+                      "eventName": eventNameField.text! as String,
+                      "eventDescription": eventDescriptionField.text! as String
+        ]
+        ref?.child("events").child(key!).setValue(event)
+        self.navigationController?.popViewController(animated: true)    }
+    
     /*
     // MARK: - Navigation
 
