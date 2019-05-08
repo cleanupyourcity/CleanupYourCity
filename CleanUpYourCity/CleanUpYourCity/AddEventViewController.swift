2f6bb8d8e1d05ffa3a6e7288145450f45ef00bb9
Jared Long <long3686@m506-118-26450.local>
2019-05-08 15:47:00 -0700

    severity level changes color, can upload severity

--- CleanUpYourCity/CleanUpYourCity/AddEventViewController.swift
+++ CleanUpYourCity/CleanUpYourCity/AddEventViewController.swift
@@ -20,17 +20,44 @@ class AddEventViewController: UIViewController {
     
     @IBOutlet weak var eventDescriptionField: UITextView!
     
+    @IBOutlet weak var eventSeverityController: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
         ref = Database.database().reference()
         
-    }
+        self.view.backgroundColor = UIColor.green    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func severityLevelChanged(_ sender: Any) {
+        
+        let eventSeverityInteger: Int = eventSeverityController.selectedSegmentIndex
+        
+        let eventSeverityLevel = String(eventSeverityInteger)
+        
+        if( eventSeverityLevel == "0")
+        {
+            self.view.backgroundColor = UIColor.green
+        }
+        else if( eventSeverityLevel == "1")
+        {
+            self.view.backgroundColor = UIColor.cyan
+        }
+        else if( eventSeverityLevel == "2")
+        {
+            self.view.backgroundColor = UIColor.yellow
+        }
+        else if( eventSeverityLevel == "3")
+        {
+            self.view.backgroundColor = UIColor.red
+        }
     }
     
     @IBAction func addEvent(_ sender: Any) {
@@ -38,12 +65,18 @@ class AddEventViewController: UIViewController {
         //Post event data to firebase
         let key = ref?.childByAutoId().key
         
+        let eventSeverityInteger: Int = eventSeverityController.selectedSegmentIndex
+        
+        let eventSeverityLevel = String(eventSeverityInteger)
+        
         //creating artist with the given values
         let event = ["eventAuthorID":userID,
                       "eventName": eventNameField.text! as String,
-                      "eventDescription": eventDescriptionField.text! as String
+                      "eventDescription": eventDescriptionField.text! as String,
+                      "eventSeverity": eventSeverityLevel
         ]
         ref?.child("events").child(key!).setValue(event)
+        ref?.child("profile").child(userID!).child("history").child(key!).setValue(event)
         self.navigationController?.popViewController(animated: true)    }
     
     /*
